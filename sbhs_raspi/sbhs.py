45c9da8c7e5bd6b9de07301a20aa88631166320f
CruiseDevice <meakashchavan@gmail.com>
2018-09-28 17:42:53 +0530

    Create Flask API for sbhs devices

--- sbhs_raspi/sbhs.py
+++ sbhs_raspi/sbhs.py
@@ -1,8 +1,6 @@
 import serial
 import os
 from time import localtime, strftime, sleep
-# import credentials
-import sbhs_server.credentials as credentials
 
 # MAP_FILE = credentials.MAP_FILE
 #LOG_FILE = '../log/sbhserr.log'
@@ -17,7 +15,7 @@ class SbhsServer(object):
         self.incoming_heat = 254
         self.outgoing_temp = 255
         self.max_heat = 100
-        self.out_max_fan = 100
+        self.max_fan = 100
 
     def get_usb_devices(self):
         usb_devices = []
@@ -27,7 +25,6 @@ class SbhsServer(object):
                     usb_devices.append(int(tty[6:]))
                 except ValueError:
                     pass
-
         return usb_devices
 
     def connect_device(self, device_num):
@@ -43,12 +40,10 @@ class SbhsServer(object):
                                           parity='N', stopbits=1,
                                           timeout=2
                                           )
-            self.sbhs = self.get_machine_id()
             # org stopbits = 1
             status = True
         except Exception as e:
             status = False
-            print(e)
         return status
 
     def map_sbhs_to_usb(self, usb_devices):
@@ -58,76 +53,9 @@ class SbhsServer(object):
                 print("usb", usb)
                 status = self.connect_device(usb)
                 if status:
-                    sbhs = self.sbhs
+                    sbhs = self.get_machine_id()
                     sbhs_map.append({"usb_id": usb, "sbhs_mac_id": sbhs})
         return sbhs_map
-
-    def connect(self, machine_id):
-        """ 
-        Open a serial connection via USB to the SBHS using the 
-        machine id 
-        """
-        # check for valid machine id number
-        try:
-            self.machine_id = int(machine_id)
-        except:
-            return False
-
-        # get the usb device file from the machine map file
-        try:
-            map_file = open(MAP_FILE, 'r')
-            usb_device_file = False
-
-            for mapping_str in map_file.readlines():
-                mapping = mapping_str.split('=')
-                self.log('mapping: ' + mapping[1], 'ERROR') #srikant
-
-                # if mapping for the machine id found set the usb device 
-                # file and break out of loop
-                if mapping[0] == str(self.machine_id):
-                    usb_device_file = mapping[1].strip()
-            #self.log('usb_device_file: ' + usb_device_file, 'ERROR') #srikant
-                    break
-            
-            # reached end of file and check if machine id entry is present 
-            # in the machine map file
-            map_file.close()
-            if not usb_device_file:
-                print("Error: cannot locate the USB device in the map table \
-                         for machine id {}".format(self.machine_id))
-                self.log('cannot locate the USB device in the map table for \
-                            machine id %d' % self.machine_id, 'ERROR')
-                return False
-        except:
-            # map_file.close()
-            print("Error: cannot get the USB device path for the machine \
-                    id {}".format(self.machine_id))
-            self.log('cannot get the USB device path for the machine id\
-                        %d' % self.machine_id, 'ERROR')
-            return False
-
-        # check if SBHS device is connected
-        if not os.path.exists(usb_device_file):
-            print("SBHS device file" +usb_device_file + "does not exists")
-            self.log('SBHS device file ' + usb_device_file + ' does not\
-                         exists', 'ERROR')
-            return False
-        try:
-            self.boardcon = serial.Serial(port=usb_device_file, \
-                                baudrate=9600, bytesize=8, parity='N', \
-                                stopbits=1, timeout=2) #orignal stopbits = 1
-            self.status = 1
-            return True
-        except serial.serialutil.SerialException:
-            print("Error: cannot connect to machine id \
-                     {}".format(self.machine_id))
-            self.log('cannot connect to machine id \
-                         %d' % self.machine_id, 'ERROR')
-            
-            self.machine_id = -1
-            self.device_num = -1
-            self.boardcon = False
-            self.status = 0
 
 
     def setHeat(self, val):
@@ -151,25 +79,20 @@ class SbhsServer(object):
                         %d' % self.machine_id, 'ERROR')
             return False
 
-    def setFan(self, val):
+    def set_fan(self, val):
         """ Sets the fan speed, checks if value is valid i.e. within range.
             Input: self object, val
             Output: Error message if fan cannot be set.
         """
-        if val > MAX_FAN or val < 0:
-            print("Error: fan value cannot be more than {}".format(MAX_FAN))
+        if val > self.out_fan or val < 0:
             return False
         try:
-            self._write(chr(INCOMING_FAN))
+            self._write(chr(self.incoming_fan))
             sleep(0.5)
             self._write(chr(val))
             return True
         except:
-            print("Error: cannot set fan for machine id \
-                    {}".format(self.machine_id))
-            self.log('cannot set fan for machine id %d' % self.machine_id, \
-                        'ERROR')
-            return False
+            return True
 
     def getTemp(self):
         """ Gets the temperature from the machine.
@@ -190,11 +113,10 @@ class SbhsServer(object):
         """ Gets machine id from the device """
         try:
             self.boardcon.flushInput()
-            self._write(str(self.outgoing_machine_id).encode())
+            self._write(chr(self.outgoing_machine_id))
             sleep(0.5) 
-            machine_id = self._read(1)
+            machine_id = ord(self._read(1))
         except Exception as e:
-            print("e-->", e)
             machine_id = -1
         return int(machine_id)
 
@@ -215,18 +137,17 @@ class SbhsServer(object):
 
     def _read(self, size):
         try:
-            data = self.boardcon.read(size).decode("ascii")
-            print(data)
+            data = self.boardcon.read(size)
             return data
         except Exception as e:
-            print(e)
+            raise
 
     def _write(self, data):
         try:
             self.boardcon.write(data)
             return True
         except Exception as e:
-            print(e)
+            raise
 
     def log(self, msg, level):
         try:
@@ -234,42 +155,10 @@ class SbhsServer(object):
             if not errfile:
                 return
             log_msg = '%s %s %s\n' %(level, strftime('%d:%m:%Y %H:%M:%S', \
-                        localtime()), msg)
+                        localtime()), msg)  
 
             errfile.write(log_msg)
             errfile.close()
             return
         except:
             return
-
-
-# if no device filename found then exit
-# for device in device_files:
-#     s = sbhs.Sbhs()
-#     # getting the number from the device filename
-#     dev_id = device[6:]
-#     try:
-#         dev_id = int(dev_id)
-#     except:
-#         print('Invalid device name /dev/%s' % device)
-#         continue
-#     # connect to device
-#     res = s.connect_device(dev_id)
-#     if not res:
-#         print ('Cannot connect to /dev/%s' % device)
-#         s.disconnect()
-#         continue
-#     # get the machine id from the device
-#     machine_id = s.getMachineId()
-#     if machine_id < 0:
-#         print('Cannot get machine id from /dev/%s' % device)
-#         s.disconnect()
-#         continue
-#     print ('Found SBHS device /dev/%s with machine id %d' % (device, machine_id))
-#     map_str = "%d=/dev/%s\n" % (machine_id, device)
-#     map_machine_file.write(map_str)
-
-# print ('Done. Exiting...')
-# map_machine_file.close()
-# sys.exit(1)
-
