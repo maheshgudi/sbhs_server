c203cc6db1648e106ec77056c8f1d4ce54095cb1
CruiseDevice <meakashchavan@gmail.com>
2018-10-26 13:35:09 +0530

    add UserBoard model to admin.py

--- sbhs/admin.py
+++ sbhs/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from sbhs.models import (Board,	Slot, Experiment, Profile)
+from sbhs.models import (Board,	Slot, Experiment, Profile, UserBoard)
 
 admin.site.register(Board)
 admin.site.register(Profile)
 admin.site.register(Slot)
 admin.site.register(Experiment)
+admin.site.register(UserBoard)
